b8070d1e899f6eaad849b3de8ba4177cf0e75f07
doujouDC <doujou.dc@gmail.com>
2019-10-17 16:10:35 -0400

    updated files and output figure 1

--- plot1.R
+++ plot1.R
@@ -5,3 +5,14 @@ library(tidyverse)
 household_power_consumption <- read_csv("data/household_power_consumption.csv", 
                                         col_types = cols(Date = col_date(format = "%d/%m/%Y"), 
                                                          Time = col_time(format = "%H:%M:%S")))
+
+plot1 <- ggplot(data = household_power_consumption, mapping = aes(x = Global_active_power)) +
+  geom_histogram(col = "black", fill = "red") +
+  labs(title = "Global Active Power",
+       x = "Global Active Power (kilowatts)",
+       x = "Frequency"
+       ) +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  theme_classic()
+
+ggsave("figure/plot1.png", plot1, width = 15, height = 10, dpi = 600)
